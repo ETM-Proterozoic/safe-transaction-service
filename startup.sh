4499cc4e27c5e1b7fc7267aed58fef3d145e583c
dexiang <52363496+little-evan-yin@users.noreply.github.com>
2022-06-24 15:35:14 +0800

    add startup shells

---
+++ startup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export DJANGO_SETTINGS_MODULE=config.settings.production
+export DJANGO_DOT_ENV_FILE=.env
+export DJANGO_READ_DOT_ENV_FILE=True
+
+# startup worker for the first time to create tables
+bash startup_worker.sh
+
+# kill worker
+sleep 20
+ps axu|grep celery |grep -v 'grep' |awk '{print $2}' |xargs kill -9
+
+# startup web
+bash startup_web.sh
+
+# startup worker
+echo "==> $(date +%H:%M:%S) ==> startup worker...  <=="
+bash startup_worker.sh
+
+# startup scheduler and flow
+bash startup_scheduler.sh
--- startup_web.sh
+++ startup_web.sh
@@ -6,19 +6,9 @@ echo "==> $(date +%H:%M:%S) ==> Collecting statics... "
 python manage.py collectstatic --noinput
 
 export DJANGO_DOT_ENV_FILE=
-export DJANGO_SETTINGS_MODULE=config.settings.production
-export DJANGO_SECRET_KEY='Very-secure-secret-string'
-export C_FORCE_ROOT=true
-export DEBUG=0
-export DATABASE_URL=psql://postgres:a~v1LE1aibzw%*456@etmp-test-postgres.cwiv8epvpytp.ap-northeast-1.rds.amazonaws.com:5432/safe_service6
-export ETHEREUM_NODE_URL=https://rinkeby.infura.io/v3/4bee79250a3148f3a4f9df00ed6a742b
-export ETH_L2_NETWORK=1
-export REDIS_URL=redis://etmp-test-redis.c2eqmv.ng.0001.apne1.cache.amazonaws.com:6379/0
-export CELERY_BROKER_URL=redis://etmp-test-redis.c2eqmv.ng.0001.apne1.cache.amazonaws.com:6379/0
-export CELERY_RESULT_BACKEND=redis://etmp-test-redis.c2eqmv.ng.0001.apne1.cache.amazonaws.com:6379/0
 
 echo "==> $(date +%H:%M:%S) ==> Send via Slack info about service version and network"
 python manage.py send_slack_notification
 
 echo "==> $(date +%H:%M:%S) ==> Running Gunicorn..."
-gunicorn --config gunicorn.conf.py -b 0.0.0.0:8888 config.wsgi:application
+gunicorn --config gunicorn.conf.py -b 0.0.0.0:8888 config.wsgi:application -D
